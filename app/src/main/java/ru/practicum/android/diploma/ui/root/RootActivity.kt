7a580c7cbd87875ed8880dabdcd68fd0335cf3e4
Andrei Urich <andrei.urich@yandex.ru>
2024-11-09 16:06:45 +0400

    Исправил ошибки "детекта"-2

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -3,23 +3,17 @@ package ru.practicum.android.diploma.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 
+
 class RootActivity : AppCompatActivity(R.layout.activity_root) {
-
-    private lateinit var bottomNavView: BottomNavigationView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
-
         val navController = navHostFragment.navController
-        bottomNavView = findViewById(R.id.bnView)
-
     }
 }
 
