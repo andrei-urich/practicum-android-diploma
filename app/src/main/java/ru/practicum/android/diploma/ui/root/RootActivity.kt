b2d29595b1f107966be6723511faf72a440df499
Andrei Urich <andrei.urich@yandex.ru>
2024-11-09 14:35:30 +0400

    Добавил контейнер под фрагменты в ативити (в код и верстку) Добавил в верстку botton_nav_view Создал пустой navigation

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,20 +2,24 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 
-class RootActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity(R.layout.activity_root) {
+
+    private lateinit var bottomNavView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
 
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        bottomNavView = findViewById(R.id.bnView)
+
     }
-
-    private fun networkRequestExample(accessToken: String) {
-        // ...
-    }
-
 }
+
