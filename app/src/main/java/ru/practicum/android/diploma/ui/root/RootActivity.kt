7e1f7334e99cf0d19e92e6ce85002e4cc4a962d8
Andrei Urich <andrei.urich@yandex.ru>
2024-11-10 13:25:35 +0400

    Настроил Bottom_Nav_View, переключение экранов.

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -3,6 +3,8 @@ package ru.practicum.android.diploma.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 
 class RootActivity : AppCompatActivity(R.layout.activity_root) {
@@ -12,6 +14,8 @@ class RootActivity : AppCompatActivity(R.layout.activity_root) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         val navController = navHostFragment.navController
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bnView)
+        bottomNavView.setupWithNavController(navController)
     }
 }
 
