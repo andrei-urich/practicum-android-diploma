f695ae90e5740ca462500e526216f27787960abc
dimoonoomid <dimoonoomid@yandex.ru>
2024-11-10 14:46:26 +0300

    DB класс добавлен

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AjsAppDatabase.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    version = 1,
+    entities = []
+)
+abstract class AjsAppDatabase : RoomDatabase()
