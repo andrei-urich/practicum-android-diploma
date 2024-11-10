b613795b0012c99ee31b4b16ab1743d2acb81750
dimoonoomid <dimoonoomid@yandex.ru>
2024-11-10 12:54:50 +0300

    Добавлена инициализация класса проверки интернет подключения в DI

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,8 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.util.InternetAccessChecker
 
-val dataModule = module {}
+val dataModule = module {
+    single { InternetAccessChecker() }
+}
