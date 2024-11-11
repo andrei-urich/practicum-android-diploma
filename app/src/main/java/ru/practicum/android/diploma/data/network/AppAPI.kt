84bcaefd332661fb6997d54d0ca5bff28faae60c
Andrei Urich <andrei.urich@yandex.ru>
2024-11-11 19:31:04 +0400

    Перенес аннотацию @Headers для каждого метода в АПИ

--- app/src/main/java/ru/practicum/android/diploma/data/network/AppAPI.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/AppAPI.kt
@@ -8,15 +8,15 @@ import retrofit2.http.Query
 import ru.practicum.android.diploma.util.HH_REQUEST_HEADER
 
 interface AppAPI {
-    @GET("/vacancies")
     @Headers(HH_REQUEST_HEADER)
+    @GET("/vacancies")
     suspend fun search(
         @Header("Authorization: Bearer") token: String,
         @Query("search input") text: String
     ): VacanciesResponse
 
-    @GET("/vacancies/{vacancy_id}")
     @Headers(HH_REQUEST_HEADER)
+    @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancy(
         @Header("Authorization: Bearer") token: String,
         @Path("vacancy_id") id: String
