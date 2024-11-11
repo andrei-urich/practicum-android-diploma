ea3531fc1099f4d70238cd25c1af6e09ba21d018
Andrei Urich <andrei.urich@yandex.ru>
2024-11-11 19:52:16 +0400

    Правка детект-1

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -4,30 +4,36 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.practicum.android.diploma.data.utils.InternetAccessChecker
 import ru.practicum.android.diploma.util.ACCESS_TOKEN
+import ru.practicum.android.diploma.util.RESULT_CODE_ERROR
+import ru.practicum.android.diploma.util.RESULT_CODE_NOT_FOUND
+import ru.practicum.android.diploma.util.RESULT_CODE_OK
 
 class RetrofitNetworkClient(
-    private val apiService: AppAPI, private val internetAccessChecker: InternetAccessChecker
+    private val apiService: AppAPI,
+    private val internetAccessChecker: InternetAccessChecker
 ) : NetworkClient {
+    private var resultCode: Int = 0
+
     override suspend fun doRequest(dto: Any): Response {
         if (internetAccessChecker.isConnected()) {
             when (dto) {
                 is VacancySearchRequest -> {
                     val response = withContext(Dispatchers.IO) {
                         apiService.search(
-                            token = ACCESS_TOKEN, dto.request
+                            token = ACCESS_TOKEN,
+                            dto.request
                         )
                     }
-                    return response.apply { resultCode = 200 }
+                    return response.apply { resultCode = RESULT_CODE_OK }
                 }
 
                 else -> {
-                    return Response().apply { resultCode = 400 }
+                    resultCode = RESULT_CODE_NOT_FOUND
                 }
-
             }
         } else {
-            return Response().apply { resultCode = -1 }
+            resultCode = RESULT_CODE_ERROR
         }
+        return Response().apply { resultCode }
     }
 }
-
