31ded27f4770947316d8f41089e0ae1e40439717
CNJerry <syava11212@yandex.ru>
2024-11-15 10:00:09 +0300

    Подготовил основу для Presentation и Ui

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancydetails/VacancyDetailsViewModel.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.presentation.vacancydetails
+
+class VacancyDetailsViewModel {
+
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancydetails/VacancyDetailsState.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.ui.vacancydetails
+
+sealed interface VacancyDetailsState {
+
+}
