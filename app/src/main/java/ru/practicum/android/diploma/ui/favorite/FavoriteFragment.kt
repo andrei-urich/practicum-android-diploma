1d1386ffe73f93c8f4d8efa9f5035f7fcdcc0f5f
Dmitriy Kuznetsov <dimoonoomid@yandex.ru>
2024-11-16 16:33:32 +0300

    build issues fixed #45

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteFragment.kt
@@ -5,13 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.presentation.favorite.FavoriteViewModel
 
 class FavoriteFragment : Fragment() {
-
-    private val viewModel: FavoriteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +19,6 @@ class FavoriteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getLiveData()
+
     }
 }
