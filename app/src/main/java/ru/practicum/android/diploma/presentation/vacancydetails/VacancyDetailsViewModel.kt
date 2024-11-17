fc045f0ed8df62d5322f99986ccd42c9022e21a4
CNJerry-IvanovVyacheslav <syava11212@yandex.ru>
2024-11-17 13:40:06 +0300

    Исправление ошибок

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancydetails/VacancyDetailsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancydetails/VacancyDetailsViewModel.kt
@@ -4,11 +4,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.domain.vacancydetails.api.DetailsInteractor
-import ru.practicum.android.diploma.ui.vacancydetails.VacancyDetailsState
+import ru.practicum.android.diploma.domain.vacancydetails.api.VacancyDetailsInteractor
+import ru.practicum.android.diploma.presentation.vacancydetails.model.VacancyDetailsState
 
 class VacancyDetailsViewModel(
-    private val vacancyDetailsInteractor: DetailsInteractor,
+    private val vacancyDetailsInteractor: VacancyDetailsInteractor,
 ) : ViewModel() {
     private val vacancyDetailsState = MutableLiveData<VacancyDetailsState>()
 
