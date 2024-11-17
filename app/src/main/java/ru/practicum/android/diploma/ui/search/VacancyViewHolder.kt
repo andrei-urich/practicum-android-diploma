f149355826f9b3a84449f87eaf94e2e05a4fabc1
Andrei Urich <andrei.urich@yandex.ru>
2024-11-17 15:24:34 +0400

    detekt-1

--- app/src/main/java/ru/practicum/android/diploma/ui/search/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/VacancyViewHolder.kt
@@ -1,9 +1,6 @@
 package ru.practicum.android.diploma.ui.search
 
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.VacancyItemBinding
 import ru.practicum.android.diploma.domain.search.models.VacancyShort
 
@@ -20,14 +17,5 @@ class VacancyViewHolder(
     fun bind(vacancy: VacancyShort) {
         binding.vacancyNameAndCityTVRecycler.text = vacancy.name
         binding.companyNameTVRecycler.text = vacancy.vacancyId
-//        binding.salary.text = vacancy.salary
-
-//        Glide.with(binding.logo)
-//            .load(vacancy.artLink)
-//            .placeholder(R.drawable.logo_placeholder)
-//            .fitCenter()
-//            .transform(RoundedCorners(2))
-//            .dontAnimate()
-//            .into(binding.logo)
     }
 }
