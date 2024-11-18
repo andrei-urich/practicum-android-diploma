c5e2a600f78d0c90e9291885ad17c08119252e61
Andrei Urich <andrei.urich@yandex.ru>
2024-11-18 17:32:17 +0400

    Исправлено: - вариант "Зарплата не указана" - убрал неиспользуемый код

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchViewModel.kt
@@ -23,14 +23,9 @@ class SearchViewModel(
     private var searchJob: Job? = null
     private var searchStateLiveData = MutableLiveData<SearchState>()
     private var openTrigger = SingleEventLiveData<VacancyShort>()
-//    private var clearScreenState = MutableLiveData<Boolean>()
 
     fun getSearchStateLiveData(): LiveData<SearchState> = searchStateLiveData
     fun getOpenTrigger(): LiveData<VacancyShort> = openTrigger
-
-//    // Отвечает за очистку экрана после сброса поиска
-//    fun getClearScreenState(): LiveData<Boolean> = clearScreenState
-
     fun getSearchText(searchText: String) {
         this.searchText = searchText
         if (searchText.isNotBlank()) {
@@ -67,10 +62,6 @@ class SearchViewModel(
             }
         }
     }
-
-//    fun onSearchTextChanged(state: Boolean) {
-//       clearScreenState.postValue(state)
-//    }
 
     private fun clickDebounce(): Boolean {
         val current = isClickAllowed
