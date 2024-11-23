f5629faf3c7d17d06fd3dd0f4ad2672c32c37c54
Dmitriy Kuznetsov <dimoonoomid@yandex.ru>
2024-11-23 17:36:08 +0300

    DI Для filters

--- app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersInteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersInteractorModule.kt
@@ -1,5 +1,11 @@
 package ru.practicum.android.diploma.di.filters
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.filters.filters.api.ControlFiltersInteractor
+import ru.practicum.android.diploma.domain.filters.filters.api.ControlFiltersInteractorImpl
+import ru.practicum.android.diploma.domain.search.models.CheckFiltersUseCase
 
-val filtersInteractorModule = module { }
+val filtersInteractorModule = module {
+    single<ControlFiltersInteractor> { ControlFiltersInteractorImpl(get()) }
+    single<CheckFiltersUseCase> { ControlFiltersInteractorImpl(get()) }
+}
--- app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersRepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersRepositoryModule.kt
@@ -1,5 +1,9 @@
 package ru.practicum.android.diploma.di.filters
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.filters.filters.FiltersControlRepositoryImpls
+import ru.practicum.android.diploma.domain.filters.filters.repository.FiltersControlRepository
 
-val FiltersRepositoryModule = module { }
+val filtersRepositoryModule = module {
+    single<FiltersControlRepository> { FiltersControlRepositoryImpls(get(), get()) }
+}
