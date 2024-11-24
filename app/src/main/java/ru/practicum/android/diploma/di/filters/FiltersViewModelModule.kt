6a92cbecf386e8dae30d3d859931cd7cb630b745
Andrei Urich <andrei.urich@yandex.ru>
2024-11-24 19:55:43 +0400

    Ошибки детекта -2

--- app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersViewModelModule.kt
@@ -2,11 +2,11 @@ package ru.practicum.android.diploma.di.filters
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.presentation.filters.FilterSettingsViewModel
 import ru.practicum.android.diploma.presentation.filters.area.AreaFilterViewModel
 import ru.practicum.android.diploma.presentation.filters.area.CountryFilterViewModel
 import ru.practicum.android.diploma.presentation.filters.area.RegionFilterViewModel
 import ru.practicum.android.diploma.presentation.filters.industry.IndustryFilterViewModel
+import ru.practicum.android.diploma.presentation.filters.settings.FilterSettingsViewModel
 
 val filtersViewModelModule = module {
     viewModel { FilterSettingsViewModel(get(), get()) }
