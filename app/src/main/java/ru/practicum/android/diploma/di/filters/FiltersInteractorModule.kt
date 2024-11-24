6200b7173e6a1c7d1df1e9b67b5052db3cfc347d
CNJerry <syava11212@yandex.ru>
2024-11-24 11:17:11 +0300

    detekt-1

--- app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersInteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersInteractorModule.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.di.filters
 
 import org.koin.dsl.module
-import ru.practicum.android.diploma.domain.filters.industry.api.IndustryFilterInteractor
-import ru.practicum.android.diploma.domain.filters.industry.impl.IndustryFilterInteractorImpl
 import ru.practicum.android.diploma.domain.filters.filters.api.ControlFiltersInteractor
 import ru.practicum.android.diploma.domain.filters.filters.api.ControlFiltersInteractorImpl
+import ru.practicum.android.diploma.domain.filters.industry.api.IndustryFilterInteractor
+import ru.practicum.android.diploma.domain.filters.industry.impl.IndustryFilterInteractorImpl
 import ru.practicum.android.diploma.domain.search.models.CheckFiltersUseCase
 
 val filtersInteractorModule = module {
--- app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersRepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/filters/FiltersRepositoryModule.kt
@@ -2,10 +2,10 @@ package ru.practicum.android.diploma.di.filters
 
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import ru.practicum.android.diploma.domain.filters.industry.api.IndustryFilterRepository
-import ru.practicum.android.diploma.domain.filters.industry.impl.IndustryFilterRepositoryImpl
 import ru.practicum.android.diploma.data.filters.filters.FiltersControlRepositoryImpls
 import ru.practicum.android.diploma.domain.filters.filters.repository.FiltersControlRepository
+import ru.practicum.android.diploma.domain.filters.industry.api.IndustryFilterRepository
+import ru.practicum.android.diploma.domain.filters.industry.impl.IndustryFilterRepositoryImpl
 
 val FiltersRepositoryModule = module {
     single<IndustryFilterRepository> {
