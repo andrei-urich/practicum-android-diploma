f0b01ea35ee6d49e2445ac9daf286baad5564911
Andrei Urich <andrei.urich@yandex.ru>
2024-11-25 17:18:21 +0400

    Сделал Дата и Домейн -слои для получения Списка Стран

--- app/src/main/java/ru/practicum/android/diploma/data/filters/area/dto/AreaFilterDTO.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.data.filters.area.dto
-
-data class AreaFilterDTO(
-    val first: Int,
-    val second: Int,
-)
--- app/src/main/java/ru/practicum/android/diploma/presentation/filters/area/AreaFilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/area/AreaFilterViewModel.kt
@@ -1,11 +1,12 @@
 package ru.practicum.android.diploma.presentation.filters.area
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.domain.filters.area.AreaFilterInteractor
 
-class AreaFilterViewModel : ViewModel() {
-    private val stateLiveData = MutableLiveData<AreaState>()
-
-    fun getStateLiveData(): LiveData<AreaState> = stateLiveData
+class AreaFilterViewModel(
+    val interactor: AreaFilterInteractor
+) : ViewModel() {
+    fun getCountryList() {
+        println()
+    }
 }
