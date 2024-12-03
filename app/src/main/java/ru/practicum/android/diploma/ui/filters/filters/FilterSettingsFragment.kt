6026b7c00a7976c7d0f78dcd9d39d556db58b007
Dmitriy Kuznetsov <dimoonoomid@yandex.ru>
2024-12-03 23:51:32 +0300

    скукожил метод, в детект пролезает

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/filters/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/filters/FilterSettingsFragment.kt
@@ -8,11 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
@@ -37,11 +39,11 @@ class FilterSettingsFragment : Fragment() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 saveSalaryTargetDebounce(s.toString())
-                if (s.isNullOrEmpty() && !binding.textInputEditTextSalary.isFocused) {
-                    binding.textInputLayoutSalary.defaultHintTextColor =
+                if (s.isNullOrEmpty() && !binding.textInputETSlry.isFocused) {
+                    binding.textInputLTSlry.defaultHintTextColor =
                         ColorStateList.valueOf(requireContext().getColor(R.color.grey))
-                } else if (!binding.textInputEditTextSalary.isFocused) {
-                    binding.textInputLayoutSalary.defaultHintTextColor =
+                } else if (!binding.textInputETSlry.isFocused) {
+                    binding.textInputLTSlry.defaultHintTextColor =
                         ColorStateList.valueOf(requireContext().getColor(R.color.black_universal))
                 }
             }
@@ -64,105 +66,59 @@ class FilterSettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getIsFiltersOnLD().observe(viewLifecycleOwner) { if (it) showClearButton() else hideClearButton() }
+        viewModel.getIsFiltersOnLD().observe(viewLifecycleOwner) { makeVisibleOrGone(binding.btClear, it) }
         viewModel.getFiltersConfigurationLD().observe(viewLifecycleOwner) { renderFilters(it) }
-        viewModel.getIsFiltresChangedLD().observe(viewLifecycleOwner) {
-            if (it) showApplyButton() else hideApplyButton()
-        }
+        viewModel.getIsFiltresChangedLD().observe(viewLifecycleOwner) { makeVisibleOrGone(binding.btApply, it) }
         viewModel.getFiltersConfiguration()
-        binding.edIndustry.addTextChangedListener {
-            if (it.isNullOrEmpty()) {
-                makeGone(binding.clearIndustryButton)
-                binding.edIndustryLayout.defaultHintTextColor =
-                    ColorStateList.valueOf(requireContext().getColor(R.color.grey))
-            } else {
-                makeVisible(binding.clearIndustryButton)
-                binding.edIndustryLayout.defaultHintTextColor =
-                    ColorStateList.valueOf(requireContext().getColor(R.color.black_day_night))
-            }
-        }
-
-        binding.edWorkPlace.addTextChangedListener {
-            if (it.isNullOrEmpty()) {
-                makeGone(binding.clearAreaButton)
-                binding.edWorkPlaceLayout.defaultHintTextColor =
-                    ColorStateList.valueOf(requireContext().getColor(R.color.grey))
-            } else {
-                makeVisible(binding.clearAreaButton)
-                binding.edWorkPlaceLayout.defaultHintTextColor =
-                    ColorStateList.valueOf(requireContext().getColor(R.color.black_day_night))
-            }
-        }
-        binding.textInputEditTextSalary.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                binding.textInputLayoutSalary.defaultHintTextColor =
-                    ColorStateList.valueOf(requireContext().getColor(R.color.blue))
-            } else {
-                if (binding.textInputEditTextSalary.text.isNullOrEmpty()) {
-                    binding.textInputLayoutSalary.defaultHintTextColor =
-                        ColorStateList.valueOf(requireContext().getColor(R.color.grey))
-                } else {
-                    binding.textInputLayoutSalary.defaultHintTextColor =
-                        ColorStateList.valueOf(requireContext().getColor(R.color.black_universal))
+        with(binding) {
+            edIndustry.addTextChangedListener { clrArIndLT(it, edIndustryLayout, clearIndustryButton) }
+            edWorkPlace.addTextChangedListener { clrArIndLT(it, edWorkPlaceLayout, clearAreaButton) }
+            textInputETSlry.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) { setHintColor(textInputLTSlry, R.color.blue) } else {
+                    if (textInputETSlry.text.isNullOrEmpty()) {
+                        setHintColor(textInputLTSlry, R.color.grey)
+                    } else { setHintColor(textInputLTSlry, R.color.black_universal) }
                 }
             }
         }
-        binding.textInputEditTextSalary.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                binding.textInputLayoutSalary.clearFocus()
-            }
+        binding.textInputETSlry.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) binding.textInputLTSlry.clearFocus()
             false
         }
         binding.clearAreaButton.setOnClickListener { viewModel.clearAreas() }
         binding.clearIndustryButton.setOnClickListener { viewModel.clearIndustry() }
-        binding.checkBoxSalary.setOnClickListener {
-            viewModel.saveSalaryShowCheckFilter(binding.checkBoxSalary.isChecked)
-        }
-        binding.textInputEditTextSalary.addTextChangedListener(textWatcher)
+        binding.checkBoxSalary.setOnClickListener { viewModel.saveSalaryCheck(binding.checkBoxSalary.isChecked) }
+        binding.textInputETSlry.addTextChangedListener(textWatcher)
         binding.backFromFilter.setOnClickListener { backPress() }
         requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                backPress()
-            }
+            override fun handleOnBackPressed() { backPress() }
         })
-        binding.edWorkPlace.setOnClickListener {
-            findNavController().navigate(R.id.action_filterSettingsFragment_to_areaFilterFragment)
-        }
-        binding.edIndustry.setOnClickListener {
-            findNavController().navigate(R.id.action_filterSettingsFragment_to_industryFilterFragment)
-        }
-        binding.btClear.setOnClickListener { viewModel.clearFilters() }
-        binding.btApply.setOnClickListener {
-            viewModel.fixFiltres()
-            viewModel.forceSearch()
-            findNavController().navigateUp()
+        with(binding) {
+            edWorkPlace.setOnClickListener { findNavController().navigate(R.id.action_fltrSttngsFT_to_arFltrFT) }
+            edIndustry.setOnClickListener { findNavController().navigate(R.id.action_fltrSttngsFT_to_indFltrFT) }
+            btClear.setOnClickListener { viewModel.clearFilters() }
+            btApply.setOnClickListener {
+                viewModel.fixFiltres()
+                viewModel.forceSearch()
+                findNavController().navigateUp()
+            }
         }
     }
-
-    private fun hideClearButton() { makeGone(binding.btClear) }
-    private fun showClearButton() { makeVisible(binding.btClear) }
-    private fun hideApplyButton() { makeGone(binding.btApply) }
-    private fun showApplyButton() { makeVisible(binding.btApply) }
     private fun renderFilters(filterUI: FiltersUIModel) {
         with(binding) {
             edWorkPlace.setText(filterUI.areaNCity)
             edIndustry.setText(filterUI.industry)
-            textInputEditTextSalary.setText(filterUI.salaryTarget)
+            textInputETSlry.setText(filterUI.salaryTarget)
             checkBoxSalary.isChecked = filterUI.salaryShowChecked
         }
     }
-    private fun makeVisible(view: View) {
-        view.visibility = View.VISIBLE
-    }
-
-    private fun makeGone(view: View) {
-        view.visibility = View.GONE
+    private fun makeVisibleOrGone(view: View, visFlag: Boolean) {
+        if (visFlag) view.visibility = View.VISIBLE else view.visibility = View.GONE
     }
     private fun backPress() {
         viewModel.fixFiltres()
         findNavController().navigateUp()
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
@@ -170,5 +126,17 @@ class FilterSettingsFragment : Fragment() {
                 findNavController().navigateUp()
             }
         })
+    }
+    private fun setHintColor(tIL: TextInputLayout, clrId: Int) {
+        tIL.defaultHintTextColor = ColorStateList.valueOf(requireContext().getColor(clrId))
+    }
+    private fun clrArIndLT(text: Editable?, textLayout: TextInputLayout, clrButton: ImageView) {
+        if (text.isNullOrEmpty()) {
+            makeVisibleOrGone(clrButton, false)
+            setHintColor(textLayout, R.color.grey)
+        } else {
+            makeVisibleOrGone(clrButton, true)
+            setHintColor(textLayout, R.color.black_day_night)
+        }
     }
 }
